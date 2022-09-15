c064658fb608a991e6cc7c3cebbcbc79bee28c2b
C. Morgan Montague <morgan@wmlex.com>
2022-09-14 22:44:45 -0400

    added clock
    added a clock by getting new date, then getting hours and minutes.
    
    Also then added a standardize function

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+let clockTime = document.getElementById('clock');
+console.log(clockTime.innerText)
+let milTime = new Date()
+console.log(milTime);
+let milHours = milTime.getHours()
+console.log(milHours)
+let mint = milTime.getMinutes()
+console.log(mint)
+clockTime.innerText = milHours + ':' + mint
+console.log(clockTime.innerText)
+
+let stanHours = milHours
+console.log(stanHours)
+function standardTime() {
+    console.log('worked')
+    if (milHours >= 13) {
+        console.log('true')
+        stanHours = (milHours - 12) + ':' + mint + ' PM'
+        console.log(stanHours)
+    }
+    else if (milHours <= 12) {
+    stanHours = milHours+ ':' + mint + ' PM'
+    }
+    else {console.log('wrong')} 
+}
+standardTime()
+
+console.log(stanHours +':' + mint + ' PM')
+
+let clockStand = document.getElementById('standardTimeDiv')
+console.log(clockStand.innerText)
+clockStand.innerText = stanHours
