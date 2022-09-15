d8ea79d49424e3444c085822c54c0e8a549cad45
C. Morgan Montague <morgan@wmlex.com>
2022-09-15 18:07:57 -0400

    both clocks work
    added seconds to both running clocks

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,70 @@
-let clockTime = document.getElementById('clock');
-console.log(clockTime.innerText)
+// let clockTime = document.getElementById('clock');
 let milTime = new Date()
-console.log(milTime);
 let milHours = milTime.getHours()
-console.log(milHours)
 let mint = milTime.getMinutes()
-console.log(mint)
-clockTime.innerText = milHours + ':' + mint
-console.log(clockTime.innerText)
+let secs = milTime.getSeconds()
+// clockTime.innerText = milHours + ':' + mint + ':' + secs
 
-let stanHours = milHours
-console.log(stanHours)
+
+
+
+
+function militaryTime() {
+    let officalTime = new Date()
+    let setTime = document.getElementById('clock')
+    fhours = officalTime.getHours()
+    testmints = officalTime.getMinutes()
+    testsecs = officalTime.getSeconds()
+    let fmints = testmints
+    if (testmints < 10) {
+        fmints = '0' + testmints
+    }
+    else if (fhours === 0) {
+        fhours = 12
+    }
+    else {
+        fmints = testmints
+    }
+    let fsecs = testsecs
+    if (testsecs < 10) {
+        fsecs = '0' + testsecs
+    }
+
+    setTime.innerText = `${fhours}:${fmints}:${fsecs}`
+
+}
+
+
+setInterval(militaryTime, 1000)
+militaryTime()
+
+
 function standardTime() {
-    console.log('worked')
+    let milTime = new Date()
+    let clockStand = document.getElementById('standardTimeDiv')
+    let stanHours = milHours
+    let seconds = milTime.getSeconds()
     if (milHours >= 13) {
-        console.log('true')
-        stanHours = (milHours - 12) + ':' + mint + ' PM'
-        console.log(stanHours)
+    let zeroMints = mint
+    if (mint < 10) {
+        zeroMints = '0'+mint
+    }
+        stanHours = (milHours - 12) + ':' + zeroMints + ':' + seconds + ' PM'
     }
     else if (milHours <= 12) {
-    stanHours = milHours+ ':' + mint + ' PM'
+    stanHours = milHours+ ':' + zeroMints + ':' + seconds + ' AM'
     }
     else {console.log('wrong')}
-    
+    let zeroSeconds = seconds
+    if (seconds <10) {
+        zeroseconds = '0'+ seconds
+    }
+
+
+
+    clockStand.innerText = `${stanHours}`
 }
-//setTimeout(standardTime, 15000) need to add auto renew function
+
+setInterval(standardTime, 1000)
 standardTime()
 
-console.log(stanHours +':' + mint + ' PM')
-
-let clockStand = document.getElementById('standardTimeDiv')
-console.log(clockStand.innerText)
-clockStand.innerText = stanHours
